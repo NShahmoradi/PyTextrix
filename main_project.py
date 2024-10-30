1c32b261b9a430a95d010650cafe6af21b69169f
Narges Shahmoradi <Narcissus.nsh@gmail.com>
2024-10-30 16:28:05 +0330

    initial commit

---
+++ main_project.py
@@ -0,0 +1,10 @@
+questions = {
+    0:'Hi! welcome to command line interface (📌 if you want to exit at any part of the program please enter "q" and be mindful which enter the full path of your file). Are you sure wanna continue?(y/n)',
+    1:'Please enter path of your file:',
+    2:'Please enter a number for consecutive words counter:',
+    3:'Do you have any ignored words file?(y/n)',
+    4:'Please enter path of your ignored words file.(Your words in your ignored file should be separated by \',\')',
+    5:'Please enter min range for jump words:',
+    6:'Please enter max range for jump words:',
+    7:'Please enter path of your file which you want to save there:'
+}
